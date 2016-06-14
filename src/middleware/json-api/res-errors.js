de9fc23652ca1bddcafb33e5d8f26454926106c3
randym <digital.ipseity@gmail.com>
2016-06-14 14:20:32 +0900

    remove crazy

--- src/middleware/json-api/res-errors.js
+++ src/middleware/json-api/res-errors.js
@@ -1,23 +1,9 @@
-function buildErrors (serverErrors) {
-  if (!serverErrors) {
-    console.log('Unidentified error')
-    return
-  } else {
-    let errors = {}
-    serverErrors.errors.forEach((error) => {
-      errors[errorKey(error.source)] = error.title
-    })
-    return errors
-  }
-}
-
-function errorKey (source) {
-  return source.pointer.split('/').pop()
-}
-
 module.exports = {
   name: 'errors',
   error: function (payload) {
-    return buildErrors(payload.data)
+    if(!payload.data) {
+      console.log('Unidentified error');
+    }
+    return payload;
   }
 }
